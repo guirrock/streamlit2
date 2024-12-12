3d5be00a0ae471e3f031d9164360e9ec7e923785
guirrock <48563795+guirrock@users.noreply.github.com>
2024-12-12 13:03:15 -0300

    Update dashboard2.py

--- dashboard2.py
+++ dashboard2.py
@@ -71,6 +71,6 @@ perguntas_filtradas = perguntas_df[
 if not perguntas_filtradas.empty:
     st.subheader('Perguntas encontradas:')
     pergunta_destacada = row['Questões'].replace(selected_verb, f"<mark>{selected_verb}</mark>")
-    st.markdown(f"- {pergunta_destacada}", unsafe_allow_html=True)
+    st.write(f"- {pergunta_destacada}")
 else:
     st.write("Nenhuma pergunta encontrada para o verbo e categoria selecionados.")

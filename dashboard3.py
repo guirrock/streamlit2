fd129f3a83106620d90505c9894e39f56d4cff8a
guirrock <48563795+guirrock@users.noreply.github.com>
2024-12-20 14:39:55 -0300

    Update dashboard3.py

--- dashboard3.py
+++ dashboard3.py
@@ -214,7 +214,7 @@ except Exception as e:
 
 st.subheader(f"Núvem de Palavras para o verbo '{selected_verb}' e categoria '{selected_category}':")
 
-all_summary = " ".join(s for s in documents)
+all_summary = " ".join(s for s in perguntas_df['Questões'])
 # lista de stopword
 stopwords = set(STOPWORDS)
 stopwords.update(["da", "meu", "em", "você", "de", "ao", "os"])
@@ -222,7 +222,7 @@ stopwords.update(["da", "meu", "em", "você", "de", "ao", "os"])
 # gerar uma wordcloud
 wordcloud = WordCloud(stopwords=stopwords,
                       background_color="black",
-                      width=1600, height=800).generate(documents)
+                      width=1600, height=800).generate(all_summary)
 # Salvar a imagem
 wordcloud.to_file("airbnb_summary_wordcloud.png")
 

69a55386b41fdfb49b1466c6b7e10f3bde1ed328
guirrock <48563795+guirrock@users.noreply.github.com>
2024-12-20 13:46:29 -0300

    Update dashboard3.py

--- dashboard3.py
+++ dashboard3.py
@@ -14,6 +14,7 @@ import graphviz
 from io import BytesIO
 import base64
 from wordcloud import WordCloud
+import random
 
 st.set_page_config(
     layout="wide",  # Define o layout para usar a largura total

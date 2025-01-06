24835fcf417e0ca05f7a3c3bca2c9bd204d0f622
guirrock <48563795+guirrock@users.noreply.github.com>
2025-01-06 08:32:15 -0300

    Update dashboard3.py

--- dashboard3.py
+++ dashboard3.py
@@ -24,7 +24,7 @@ st.set_page_config(
 )
 
 # Título do dashboard
-st.markdown("<h1 style='text-align: center; color: red;'>Análise de Verbos por Nível da Taxonomia de Bloom</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: red;'>Análise Visual de Enunciados de Perguntas Avaliativas a partir de Verbos</h1>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center; color: black;'>Projeto Final - Disciplina - Visual Analytics for Data Science - INF/UFRGS</h3>", unsafe_allow_html=True)
 
 # Adicionar estilo CSS para a área de rolagem fixa
